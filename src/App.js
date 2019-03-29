7f3b08e25a5a8f2ac6b99c602b2990f287558b54
Gabriel <Gabriel>
2019-03-28 21:40:58 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Header from './components/Header';
+import './styles.css';
+
+
+
+const App = () => (
+	<div className='App'>
+		<Header />
+	</div>
+);
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+      
+//       // Utiliza-se className no lugar de class, pois class e utilizado pelo react
+      
+//       <div className="App">
+//         <h1>Hello world</h1>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header id='main-header'>JSHunt</header>
+);
+
+// Mesma coisa que
+
+// class Header extends Component{
+//     render(){
+//         return <h1>Hello</h1>
+//     }
+// }
+
+
+export default Header;
